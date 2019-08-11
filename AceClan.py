7d6374cda7819334d3d20056cb696b5e056ad9f2
Toaster13245 <50031154+Toaster13245@users.noreply.github.com>
2019-08-11 00:20:08 -0400

    Create AceClan.py

---
+++ AceClan.py
@@ -0,0 +1,182 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="*")
+
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await ctx.send("Pong")
+
+
+@bot.command(pass_context=True)
+async def Ping(ctx):
+    await ctx.send("Pong")
+
+
+@bot.command(pass_context=True)
+async def hello(ctx):
+    await ctx.send("Hi! :wave:")
+
+
+@bot.command(pass_context=True)
+async def Hello(ctx):
+    await ctx.send("Hi!:wave:")
+
+
+@bot.command(pass_context=True)
+async def Thot(ctx):
+    await ctx.send("***BE GONE THOT!!!***")
+
+
+@bot.command(pass_context=True)
+async def thot(ctx):
+    await ctx.send("***BE GONE THOT!!!***")
+
+
+@bot.command(pass_context=True)
+async def oof(ctx):
+    await ctx.send("***Sad Roblox Music***")
+
+
+@bot.command(pass_context=True)
+async def Oof(ctx):
+    await ctx.send("***Sad Roblox Music***")
+
+
+@bot.command(pass_context=True)
+async def Sans(ctx):
+    await ctx.send("***megalovania intensifies***")
+
+
+@bot.command(pass_context=True)
+async def sans(ctx):
+    await ctx.send("***megalovania intensifies***")
+
+
+@bot.command(pass_context=True)
+async def Fortnite(ctx):
+    await ctx.send("***IS GAY***")
+
+
+@bot.command(pass_context=True)
+async def fortnite(ctx):
+    await ctx.send("***IS GAY***")
+
+
+@bot.command(pass_context=True)
+async def ree(ctx):
+    await ctx.send("***REEEEEEEEEE***")
+
+
+@bot.command(pass_context=True)
+async def Ree(ctx):
+    await ctx.send("***REEEEEEEEEE***")
+
+
+@bot.command(pass_context=True)
+async def Creeper(ctx):
+    await ctx.send("***AWW MAN***")
+
+
+@bot.command(pass_context=True)
+async def CREEPER(ctx):
+    await ctx.send("***AWW MAN***")
+
+
+@bot.command(pass_context=True)
+async def creeper(ctx):
+    await ctx.send("***AWW MAN***")
+
+
+@bot.command(pass_context=True)
+async def F(ctx):
+    await ctx.send("**F** In The Chat Bois")
+
+
+@bot.command(pass_context=True)
+async def f(ctx):
+    await ctx.send("**F** In The Chat Bois")
+
+
+@bot.command(pass_context=True)
+async def UwU(ctx):
+    await ctx.send("**OwO**")
+
+
+@bot.command(pass_context=True)
+async def uwu(ctx):
+    await ctx.send("**OwO**")
+
+
+@bot.command(pass_context=True)
+async def annoy(ctx):
+    await ctx.send("@everyone")
+
+
+@bot.command(pass_context=True)
+async def Annoy(ctx):
+    await ctx.send("@everyone")
+
+
+@bot.command(pass_context=True)
+async def Cat(ctx):
+    await ctx.send("Meow")
+
+
+@bot.command(pass_context=True)
+async def cat(ctx):
+    await ctx.send("Meow")
+
+
+@bot.command(pass_context=True)
+async def Toaster(ctx):
+    await ctx.send("Throw it in a bathtub!")
+
+
+@bot.command(pass_context=True)
+async def toaster(ctx):
+    await ctx.send("Throw it in a bathtub!")
+
+
+@bot.command(pass_context=True)
+async def storm(ctx):
+    await ctx.send("@everyone Storm war's is starting")
+
+
+@bot.command(pass_context=True)
+async def Storm(ctx):
+    await ctx.send("@everyone Storm war's is starting")
+
+
+@bot.command(pass_context=True)
+async def Mom(ctx):
+    await ctx.send("***I NEED V-BUCKS!!!***")
+
+
+@bot.command(pass_context=True)
+async def mom(ctx):
+    await ctx.send("***I NEED V-BUCKS!!!***")
+    
+
+@bot.command(pass_context=True)
+async def tomas(ctx):
+    await ctx.send("Tomas and Tommy are both gay nigger faggots")
+
+
+@bot.command(pass_context=True)
+async def Tomas(ctx):
+    await ctx.send("Tomas and Tommy are both gay nigger faggots")
+
+
+@bot.command(pass_context=True)
+async def oxen(ctx):
+    await ctx.send("Boxen")
+
+
+@bot.command(pass_context=True)
+async def Oxen(ctx):
+    await ctx.send("Boxen")
+
+
+    
